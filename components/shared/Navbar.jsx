4e0bbd85b4f631683be82d2c34e4108107d44bcf
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-04-11 00:20:28 +0530

    changed to fit

--- components/shared/Navbar.jsx
+++ components/shared/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 					</div>
 					<ul
 						tabIndex={0}
-						className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-60'
+						className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-fit'
 					>
 						<li>
 							<Link href='/'>Home</Link>
